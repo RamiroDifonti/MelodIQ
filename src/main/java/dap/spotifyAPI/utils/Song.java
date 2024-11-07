07345ba64a560e34f34f1d4af1197befac02536f
Ramiro Difonti <alu0101425030@ull.edu.es>
2024-11-07 20:51:43 +0000

    Song modificado con lo mínimo para funcionar el main

--- src/main/java/dap/spotifyAPI/utils/Song.java
+++ src/main/java/dap/spotifyAPI/utils/Song.java
@@ -3,6 +3,9 @@ package dap.spotifyAPI.utils;
 
 import se.michaelthelin.spotify.model_objects.specification.Track;
 
+import javax.swing.*;
+import java.awt.*;
+
 // Clase que representa una canción, tiene:
 // - portada de la canción
 // - nombre de la canción
@@ -10,7 +13,12 @@ import se.michaelthelin.spotify.model_objects.specification.Track;
 // - duración de la canción ¿?
 // - url de la canción (revisar api spotify)
 public class Song {
+    JPanel _cover;
     public Song(Track song) {
-
+        _cover = new JPanel();
+        _cover.setLayout(new BoxLayout(_cover, BoxLayout.X_AXIS));
+    }
+    public JPanel getLayout() {
+        return _cover;
     }
 }
