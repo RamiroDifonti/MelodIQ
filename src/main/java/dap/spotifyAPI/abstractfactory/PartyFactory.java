786d1e54e62709672fad5917fdf647624062093c
Ramiro Difonti <alu0101425030@ull.edu.es>
2024-11-07 14:46:27 +0000

    factoria fiesta abstracta solucionada

--- src/main/java/dap/spotifyAPI/abstractfactory/PartyFactory.java
+++ src/main/java/dap/spotifyAPI/abstractfactory/PartyFactory.java
@@ -2,21 +2,22 @@ package dap.spotifyAPI.abstractfactory;
 
 import dap.spotifyAPI.products.jazz.JazzProduct;
 import dap.spotifyAPI.products.pop.PopProduct;
-import dap.spotifyAPI.products.reggaetton.ReggaetonProduct;
+import dap.spotifyAPI.products.reggaeton.PartyReggaeton;
+import dap.spotifyAPI.products.reggaeton.ReggaetonProduct;
 
 public class PartyFactory implements PlaylistFactory {
     @Override
     public PopProduct createPop() {
-        return null;
+        return new PartyPop();
     }
 
     @Override
     public JazzProduct createJazz() {
-        return null;
+        return new PartyJazz();
     }
 
     @Override
-    public ReggaetonProduct createReggaetton() {
-        return null;
+    public ReggaetonProduct createReggaeton() {
+        return new PartyReggaeton();
     }
 }
