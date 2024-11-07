d152321f8e16d0b9fefaf06ce5f3ea56966c87ae
Ramiro Difonti <alu0101425030@ull.edu.es>
2024-11-07 15:06:51 +0000

    StudyReggaeton acabado

--- src/main/java/dap/spotifyAPI/products/reggaeton/StudyReggaeton.java
+++ src/main/java/dap/spotifyAPI/products/reggaeton/StudyReggaeton.java
@@ -1,12 +1,38 @@
 package dap.spotifyAPI.products.reggaeton;
 
 import dap.spotifyAPI.utils.Song;
+import org.apache.hc.core5.http.ParseException;
 import se.michaelthelin.spotify.SpotifyApi;
+import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
+import se.michaelthelin.spotify.model_objects.specification.Paging;
+import se.michaelthelin.spotify.model_objects.specification.Track;
+import se.michaelthelin.spotify.requests.data.search.simplified.SearchTracksRequest;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class StudyReggaeton extends ReggaetonProduct {
     public List<Song> create(SpotifyApi spotifyApi, int amount) {
-        return null;
+        // Subgenero del reggaeton para estudiar
+        String subgenero = "Reggaetón R&B";
+        SearchTracksRequest searchTracksRequest = spotifyApi.searchTracks(subgenero)
+                .limit(amount)
+                .build();
+        List<Song> songs = new ArrayList<>();
+        try {
+            // Realizar la solicitud y obtener los resultados
+            Paging<Track> trackPaging = searchTracksRequest.execute();
+
+            // Procesar y crear las canciones
+            Track[] tracks = trackPaging.getItems();
+
+            for (int i = 0; i < tracks.length; i++) {
+                songs.add(new Song(tracks[i]));
+            }
+        } catch (IOException | SpotifyWebApiException | ParseException e) {
+            _logger.info("Error al obtener las canciones de reggaeton de estudiar: " + e.getMessage());
+        }
+        return songs;
     }
 }
