ab44894fd2ac68d857a7873bbf70ad2c87825d7f
Ramiro Difonti <alu0101425030@ull.edu.es>
2024-11-07 20:52:00 +0000

    GUI "acabada", falta comprobar que funcione

--- src/main/java/dap/spotifyAPI/GUI.java
+++ src/main/java/dap/spotifyAPI/GUI.java
@@ -1,14 +1,18 @@
 package dap.spotifyAPI;
 
-import dap.spotifyAPI.abstractfactory.PartyFactory;
-
+import dap.spotifyAPI.abstractfactory.PlaylistFactory;
 import dap.spotifyAPI.abstractfactory.SportFactory;
 import dap.spotifyAPI.abstractfactory.StudyFactory;
-import dap.spotifyAPI.abstractfactory.PlaylistFactory;
-import org.apache.hc.core5.http.ParseException;
+import dap.spotifyAPI.abstractfactory.PartyFactory;
+
 import dap.spotifyAPI.products.jazz.JazzProduct;
 import dap.spotifyAPI.products.pop.PopProduct;
-import dap.spotifyAPI.products.reggaetton.ReggaettonProduct;
+import dap.spotifyAPI.products.reggaeton.ReggaetonProduct;
+
+import dap.spotifyAPI.utils.Song;
+
+import org.apache.hc.core5.http.ParseException;
+
 import se.michaelthelin.spotify.SpotifyApi;
 import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
 import se.michaelthelin.spotify.requests.authorization.client_credentials.ClientCredentialsRequest;
@@ -19,6 +23,8 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 class Frame extends JFrame {
 
@@ -31,9 +37,13 @@ class Frame extends JFrame {
     private PlaylistFactory _factory;
     private int _amount;
     private String _playlistName = "Default";
+    private Boolean[] _genres = new Boolean[]{false, false, false};
 
     public Frame(SpotifyApi spotifyApi) {
         _spotifyApi = spotifyApi;
+        Start();
+    }
+    public void Start() {
         setLayout(new FlowLayout());
         // Crear un BoxLayout para ir introduciendo los objetos
         _panel = new JPanel();
@@ -74,20 +84,20 @@ class Frame extends JFrame {
         numberField.setAlignmentX(Component.CENTER_ALIGNMENT);
         _panel.add(numberField);
 
-/*        // Tipos de genero
+        // Tipos de genero
         JLabel genreTitle = new JLabel("Seleccione los tipos de género que quieres tener en la playlist:");
         genreTitle.setAlignmentX(Component.CENTER_ALIGNMENT);
         _panel.add(genreTitle);
         // Crear tres JCheckBox
-        JCheckBox pop = new JCheckBox("Botón 1");
-        JCheckBox jazz = new JCheckBox("Botón 2");
-        JCheckBox reggaeton = new JCheckBox("Botón 3");
-
-        // Añadir los JCheckBox al JFrame
-        add(pop);
-        add(jazz);
-        add(reggaeton);*/
-
+        JPanel genres = new JPanel();
+        genres.setLayout(new BoxLayout(genres, BoxLayout.X_AXIS));
+        JCheckBox pop = new JCheckBox("Pop");
+        JCheckBox jazz = new JCheckBox("Jazz");
+        JCheckBox reggaeton = new JCheckBox("Reggaeton");
+        genres.add(pop);
+        genres.add(jazz);
+        genres.add(reggaeton);
+        _panel.add(genres);
 
         // Generar playlist
         JButton confirm = new JButton("Generar");
@@ -119,21 +129,107 @@ class Frame extends JFrame {
                 } else if (party.isSelected() ) {
                     _factory = new PartyFactory();
                 } else {
-                    JOptionPane.showMessageDialog(null, "No has seleccionado ninguna opción");
+                    JOptionPane.showMessageDialog(null, "No has seleccionado ninguna opción.");
                 }
+                if (pop.isSelected()) {
+                    _genres[0] = true;
+                }
+                if (jazz.isSelected()) {
+                    _genres[1] = true;
+                }
+                if (reggaeton.isSelected()) {
+                    _genres[2] = true;
+                }
+                if (!_genres[0] && !_genres[1] && !_genres[2]) {
+                    JOptionPane.showMessageDialog(null, "No has seleccionado ningún género.");
+                }
+                createPlaylist();
             }
         });
         add(_panel);
+        revalidate();
+        repaint();
         setSize(1000, 850);
         setVisible(true);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
     }
     public void createPlaylist() {
-        JazzProduct jp = _factory.addJazz();
-        PopProduct pp = _factory.addPop();
-        ReggaettonProduct rp = _factory.addReggaetton();
+        // Eliminar y limpiar jframe
+        remove(_panel);
 
+        // Llamar a los productos
+        JazzProduct jp = _factory.createJazz();
+        PopProduct pp = _factory.createPop();
+        ReggaetonProduct rp = _factory.createReggaeton();
+
+        // Canciones de cada género
+        int sumGenres = 0;
+        for (Boolean genre : _genres) {
+            if (genre) {
+                ++sumGenres;
+            }
+        }
+        int numSongs[] = new int[] {0, 0, 0};
+        for (int i = 0; i < 3; ++i) {
+            if (_genres[i]) {
+                numSongs[i] = _amount / sumGenres;
+            }
+        }
+        if (_amount % sumGenres != 0) {
+            for (int i = 0; i < 3; ++i) {
+                if (numSongs[i] != 0) {
+                    numSongs[i] += _amount % sumGenres;
+                }
+            }
+        }
+        JPanel songsPanel = new JPanel();
+        songsPanel.setLayout(new BoxLayout(songsPanel, BoxLayout.Y_AXIS));
+        if (_genres[0]) {
+            List<Song> popSongs = pp.create(_spotifyApi, numSongs[0]);
+            for (Song song : popSongs) {
+                songsPanel.add(song.getLayout());
+            }
+        }
+        if (_genres[1]) {
+            List<Song> jazzSongs = jp.create(_spotifyApi, numSongs[1]);
+            for (Song song : jazzSongs) {
+                songsPanel.add(song.getLayout());
+            }
+        }
+        if (_genres[2]) {
+            List<Song> reggaetonSongs = rp.create(_spotifyApi, numSongs[2]);
+            for (Song song : reggaetonSongs) {
+                songsPanel.add(song.getLayout());
+            }
+        }
+        // Panel scroll que se mueve de arriba a abajo
+        JScrollPane scrollPane = new JScrollPane(songsPanel);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+
+        // Titulo de la playlist
+        JLabel playlistTitle = new JLabel(_playlistName);
+        playlistTitle.setAlignmentX(Component.CENTER_ALIGNMENT);
+        _panel.add(playlistTitle);
+        _panel.add(scrollPane);
+
+        // Botón para volver
+        // Generar playlist
+        JButton ret = new JButton("Volver");
+        ret.setAlignmentX(Component.CENTER_ALIGNMENT);
+        _panel.add(ret);
+
+        ret.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                remove(_panel);
+                Start();
+            }
+        });
+        add(_panel);
+        revalidate();
+        repaint();
     }
 }
 
