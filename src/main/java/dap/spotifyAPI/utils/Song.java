2feefbbd8505214af1b6277ec3548cb2a8ef50a8
Ramiro Difonti <alu0101425030@ull.edu.es>
2024-11-07 13:36:40 +0000

    Clase Song añadida

---
+++ src/main/java/dap/spotifyAPI/utils/Song.java
@@ -0,0 +1,11 @@
+package dap.spotifyAPI.utils;
+
+
+// Clase que representa una canción, tiene:
+// - portada de la canción
+// - nombre de la canción
+// - nombre del artista
+// - duración de la canción ¿?
+// - url de la canción (revisar api spotify)
+public class Song {
+}
