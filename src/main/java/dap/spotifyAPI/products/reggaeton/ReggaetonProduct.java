1469a4ffd1ba31cca378e1c50e59ce297cc0724f
Ramiro Difonti <alu0101425030@ull.edu.es>
2024-11-07 14:47:08 +0000

    Logger añadido para el producto abstracto

--- src/main/java/dap/spotifyAPI/products/reggaeton/ReggaetonProduct.java
+++ src/main/java/dap/spotifyAPI/products/reggaeton/ReggaetonProduct.java
@@ -1,10 +1,13 @@
 package dap.spotifyAPI.products.reggaeton;
 
 import dap.spotifyAPI.utils.Song;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import se.michaelthelin.spotify.SpotifyApi;
 
 import java.util.List;
 
 public abstract class ReggaetonProduct {
+    protected Logger _logger = LoggerFactory.getLogger(ReggaetonProduct.class);
     public abstract List<Song> create(SpotifyApi spotifyApi, int amount);
 }
