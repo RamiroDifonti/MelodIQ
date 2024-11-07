018df72ebeee029bd1586ea810c8a524f11d694d
Ramiro Difonti <alu0101425030@ull.edu.es>
2024-11-07 14:48:46 +0000

    Constructor clase song definido

--- src/main/java/dap/spotifyAPI/utils/Song.java
+++ src/main/java/dap/spotifyAPI/utils/Song.java
@@ -1,6 +1,8 @@
 package dap.spotifyAPI.utils;
 
 
+import se.michaelthelin.spotify.model_objects.specification.Track;
+
 // Clase que representa una canción, tiene:
 // - portada de la canción
 // - nombre de la canción
@@ -8,4 +10,7 @@ package dap.spotifyAPI.utils;
 // - duración de la canción ¿?
 // - url de la canción (revisar api spotify)
 public class Song {
+    public Song(Track song) {
+
+    }
 }
