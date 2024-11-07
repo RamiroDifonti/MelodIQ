909bf3dd1ecae23a02abc3c9f0319cca63c01f2a
Ramiro Difonti <alu0101425030@ull.edu.es>
2024-11-07 14:11:57 +0000

    Modificado factoría abstracta

--- src/main/java/dap/spotifyAPI/abstractfactory/PlaylistFactory.java
+++ src/main/java/dap/spotifyAPI/abstractfactory/PlaylistFactory.java
@@ -2,12 +2,12 @@ package dap.spotifyAPI.abstractfactory;
 
 import dap.spotifyAPI.products.jazz.JazzProduct;
 import dap.spotifyAPI.products.pop.PopProduct;
-import dap.spotifyAPI.products.reggaetton.ReggaettonProduct;
+import dap.spotifyAPI.products.reggaeton.ReggaetonProduct;
 
 
 public interface PlaylistFactory {
     PopProduct createPop();
     JazzProduct createJazz();
-    ReggaettonProduct createReggaetton();
+    ReggaetonProduct createReggaeton();
 
 }
