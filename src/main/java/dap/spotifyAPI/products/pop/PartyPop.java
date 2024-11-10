e9928b4067b31e0632077aee65276d8e55aacba0
mariaferrandizdiaz <mariaferrandizdiaz2003@gmail.com>
2024-11-10 22:51:46 +0000

    Creación y definición del producto concreto PartyPop

---
+++ src/main/java/dap/spotifyAPI/products/pop/PartyPop.java
@@ -0,0 +1,39 @@
+package dap.spotifyAPI.products.pop;
+
+import dap.spotifyAPI.utils.Song;
+import org.apache.hc.core5.http.ParseException;
+import org.apache.log4j.LogMF;
+import se.michaelthelin.spotify.SpotifyApi;
+import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
+import se.michaelthelin.spotify.model_objects.specification.Paging;
+import se.michaelthelin.spotify.model_objects.specification.Track;
+import se.michaelthelin.spotify.requests.data.search.simplified.SearchTracksRequest;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PartyPop extends PopProduct {
+    public List<Song> create(SpotifyApi spotifyApi, int amount) {
+        String genero = "pop";
+        List<Song> songs = new ArrayList<>();
+        SearchTracksRequest searchTracksRequest = spotifyApi.searchTracks("genre:\"" + genero + "\"")
+                .limit(amount)
+                .build();
+
+        try {
+            // Realizar la solicitud y obtener los resultados
+            Paging<Track> trackPaging = searchTracksRequest.execute();
+
+            // Procesar y mostrar los resultados
+            Track[] tracks = trackPaging.getItems();
+
+            for (Track track : tracks) {
+                songs.add(track.getName(), track.getArtists()[0].getName());
+            }
+        } catch (IOException | SpotifyWebApiException | ParseException e) {
+            _logger.info("Error al obtener canciones de pop para fiesta: " + e.getMessage());
+        }
+        return songs;
+    }
+}
