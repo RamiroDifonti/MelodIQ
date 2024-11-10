e258df2a59bdbab8719dcb5523e4394558ef5614
mariaferrandizdiaz <mariaferrandizdiaz2003@gmail.com>
2024-11-10 23:05:55 +0000

    Documentación StudyPop.java

--- src/main/java/dap/spotifyAPI/products/pop/StudyPop.java
+++ src/main/java/dap/spotifyAPI/products/pop/StudyPop.java
@@ -12,7 +12,18 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Clase concreta que define el comportamiento de los productos de tipo Pop para estudio en la aplicación.
+ * Esta clase extiende de la clase PopProduct y sobreescribe el metodo create para crear una lista de canciones
+ * de pop para estudio.
+ */
 public class StudyPop extends PopProduct {
+    /**
+     * Metodo que crea una lista de canciones de pop para estudio.
+     * @param spotifyApi Objeto que se encarga de realizar las solicitudes a la API de Spotify.
+     * @param amount Cantidad de canciones que se van a crear en la playlist.
+     * @return Lista de canciones de pop para estudio.
+     */
     public List<Song> create(SpotifyApi spotifyApi, int amount) {
         String genero = "pop";
         SearchTracksRequest searchTracksRequest = spotifyApi.searchTracks("genre:\"" + genero + "\" mood:study")
@@ -21,12 +32,13 @@ public class StudyPop extends PopProduct {
         List<Song> songs = new ArrayList<>();
 
         try {
-            // Realizar la solicitud y obtener los resultados
+            // Realizamos la solicitud y obtenemos los resultados
             Paging<Track> trackPaging = searchTracksRequest.execute();
 
-            // Procesar y mostrar los resultados
+            // Se procesan y se muestran los resultados
             Track[] tracks = trackPaging.getItems();
 
+            // Se recorren las canciones obtenidas y se muestran en consola
             for (Track track : tracks) {
                 System.out.println("Nombre: " + track.getName() + "\nArtista: " + track.getArtists()[0].getName() + "\nURI: " + track.getUri());
                 // songs.add(new Song(track.getName(), track.getArtists()[0].getName(), track.getUri()));
