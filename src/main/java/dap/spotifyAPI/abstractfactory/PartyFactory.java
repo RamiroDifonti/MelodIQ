494e51fa8ca7ace646a68764a80fc5c14880c2f7
eglezgut03 <alu0101461588@ull.edu.es>
2024-11-12 17:25:04 +0000

    Comentario PartyFactory.java

--- src/main/java/dap/spotifyAPI/abstractfactory/PartyFactory.java
+++ src/main/java/dap/spotifyAPI/abstractfactory/PartyFactory.java
@@ -7,19 +7,42 @@ import dap.spotifyAPI.products.reggaeton.PartyReggaeton;
 import dap.spotifyAPI.products.reggaeton.ReggaetonProduct;
 import dap.spotifyAPI.products.pop.PartyPop;
 
+/**
+ * Implementación concreta de la interfaz {@link PlaylistFactory} para crear productos de tipo
+ * Pop, Jazz y Reggaeton para una playlist de fiesta.
+ * Esta clase implementa los métodos de la interfaz para crear productos de diferentes géneros musicales
+ * de estilo de fiesta.
+ */
 public class PartyFactory implements PlaylistFactory {
+
+    /**
+     * Método que crea un producto de tipo Pop de estilo de fiesta.
+     *
+     * @return Un objeto de tipo {@link PopProduct} representando una playlist de fiesta en el género Pop.
+     */
     @Override
     public PopProduct createPop() {
-        return new PartyPop();
+        return new PartyPop(); // Retorna un nuevo producto de tipo Pop con estilo de fiesta.
     }
 
+    /**
+     * Método que crea un producto de tipo Jazz de estilo de fiesta.
+     *
+     * @return Un objeto de tipo {@link JazzProduct} representando una playlist de fiesta en el género Jazz.
+     */
     @Override
     public JazzProduct createJazz() {
-        return new PartyJazz();
+        return new PartyJazz(); // Retorna un nuevo producto de tipo Jazz con estilo de fiesta.
     }
 
+    /**
+     * Método que crea un producto de tipo Reggaeton de estilo de fiesta.
+     *
+     * @return Un objeto de tipo {@link ReggaetonProduct} representando una playlist de fiesta en el género Reggaeton.
+     */
     @Override
     public ReggaetonProduct createReggaeton() {
-        return new PartyReggaeton();
+        return new PartyReggaeton(); // Retorna un nuevo producto de tipo Reggaeton con estilo de fiesta.
     }
 }
+
