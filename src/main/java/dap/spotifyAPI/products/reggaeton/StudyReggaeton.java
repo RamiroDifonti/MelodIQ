314b5d954e73c8228d34c972f00cd10bd30aefb9
eglezgut03 <alu0101461588@ull.edu.es>
2024-11-12 17:18:18 +0000

    Comentario StudyReggaeton.java

--- src/main/java/dap/spotifyAPI/products/reggaeton/StudyReggaeton.java
+++ src/main/java/dap/spotifyAPI/products/reggaeton/StudyReggaeton.java
@@ -12,9 +12,27 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Clase que representa un producto de reggaetón para estudiar.
+ *
+ * Esta clase extiende {@link ReggaetonProduct} y implementa el método {@link #create(SpotifyApi, int)}
+ * para obtener una lista de canciones de reggaetón con un subgénero específico, en este caso,
+ * canciones de "Reggaetón R&B" para crear un ambiente adecuado para estudiar.
+ */
 public class StudyReggaeton extends ReggaetonProduct {
+
+    /**
+     * Crea una lista de canciones de reggaetón con el subgénero "Reggaetón R&B" para estudiar.
+     *
+     * Este método realiza una búsqueda en la API de Spotify con el subgénero "Reggaetón R&B" y
+     * devuelve una lista de canciones obtenidas según la cantidad especificada.
+     *
+     * @param spotifyApi La instancia de la API de Spotify utilizada para realizar la búsqueda de canciones.
+     * @param amount El número de canciones que se desean obtener.
+     * @return Una lista de objetos {@link Song} que representan las canciones obtenidas.
+     */
     public List<Song> create(SpotifyApi spotifyApi, int amount) {
-        // Subgenero del reggaeton para estudiar
+        // Subgénero del reggaetón para estudiar
         String subgenero = "Reggaetón R&B";
         SearchTracksRequest searchTracksRequest = spotifyApi.searchTracks(subgenero)
                 .limit(amount)
