678626996fff58dda11987adfcb45153f2c7d49e
eglezgut03 <alu0101461588@ull.edu.es>
2024-11-12 17:25:42 +0000

    Comentario PlaylistFactory.java

--- src/main/java/dap/spotifyAPI/abstractfactory/PlaylistFactory.java
+++ src/main/java/dap/spotifyAPI/abstractfactory/PlaylistFactory.java
@@ -4,10 +4,32 @@ import dap.spotifyAPI.products.jazz.JazzProduct;
 import dap.spotifyAPI.products.pop.PopProduct;
 import dap.spotifyAPI.products.reggaeton.ReggaetonProduct;
 
-
+/**
+ * Interfaz para la creación de productos de playlists de diferentes géneros musicales.
+ * Esta interfaz define los métodos para crear productos de tipo Pop, Jazz y Reggaeton.
+ * Las implementaciones concretas de esta interfaz, como {@link PartyFactory},
+ * se encargan de generar los productos adecuados según el estilo deseado.
+ */
 public interface PlaylistFactory {
-    PopProduct createPop();
-    JazzProduct createJazz();
-    ReggaetonProduct createReggaeton();
 
+    /**
+     * Método para crear un producto de tipo Pop.
+     *
+     * @return Un objeto de tipo {@link PopProduct} representando una playlist en el género Pop.
+     */
+    PopProduct createPop();
+
+    /**
+     * Método para crear un producto de tipo Jazz.
+     *
+     * @return Un objeto de tipo {@link JazzProduct} representando una playlist en el género Jazz.
+     */
+    JazzProduct createJazz();
+
+    /**
+     * Método para crear un producto de tipo Reggaeton.
+     *
+     * @return Un objeto de tipo {@link ReggaetonProduct} representando una playlist en el género Reggaeton.
+     */
+    ReggaetonProduct createReggaeton();
 }
