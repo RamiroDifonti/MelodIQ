766683acc97d64e39a4cb752e4e7197b95b580f6
Ramiro <alu0101425030@ull.edu.es>
2025-01-01 18:42:59 +0000

    playlist añadido

--- src/main/java/dap/spotifyAPI/template/Playlist.java
+++ src/main/java/dap/spotifyAPI/template/Playlist.java
@@ -1,9 +1,19 @@
 package dap.spotifyAPI.template;
 
+import se.michaelthelin.spotify.model_objects.specification.PlaylistSimplified;
+
+import java.util.List;
+
 public class Playlist extends SearchTemplate {
     @Override
     public void apiCall(String name, String searchField) {
-
+        List<PlaylistSimplified> playlists = _manager.getPlaylistsByUser(name);
+        for (PlaylistSimplified playlist : playlists) {
+            if (playlist.getName().contains(searchField)) {
+                _songs = _manager.getPlaylistTracks(playlist.getId());
+                break;
+            }
+        }
     }
 
     @Override
