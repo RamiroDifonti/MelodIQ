3d6219c7625db9c8e02e9112f006e1d180ee83ce
Ramiro <alu0101425030@ull.edu.es>
2025-01-01 15:17:12 +0000

    Plantilla concreta Playlist iniciada

---
+++ src/main/java/dap/spotifyAPI/template/Playlist.java
@@ -0,0 +1,19 @@
+package dap.spotifyAPI.template;
+
+public class Playlist extends SearchTemplate {
+    @Override
+    public void apiCall(String name, String searchField) {
+
+    }
+
+    @Override
+    public boolean hasSong() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void fetchSong() {
+        // TODO Auto-generated method stub
+    }
+}
