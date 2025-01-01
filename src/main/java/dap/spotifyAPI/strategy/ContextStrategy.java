02c94a9f33c8a527d16b0fc0c5af1560e3dcf629
PedroHdez18 <alu0101423944@ull.edu.es>
2025-01-01 18:25:24 +0000

    implementacion ContextStrategy

--- src/main/java/dap/spotifyAPI/strategy/ContextStrategy.java
+++ src/main/java/dap/spotifyAPI/strategy/ContextStrategy.java
@@ -1,4 +1,17 @@
 package dap.spotifyAPI.strategy;
 
 public class ContextStrategy {
+    private Strategy strategy;
+
+    public void setStrategy(Strategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public void executeStrategy(String input) {
+        if (strategy != null) {
+            strategy.execute(input);
+        } else {
+            System.out.println("Estrategia no definida");
+        }
+    }
 }
