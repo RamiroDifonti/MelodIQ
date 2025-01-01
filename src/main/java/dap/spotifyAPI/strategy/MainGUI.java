ee458d1155cab21507e730ea0221c761e6249cb6
PedroHdez18 <alu0101423944@ull.edu.es>
2025-01-01 18:53:27 +0000

    editando gui

--- src/main/java/dap/spotifyAPI/strategy/MainGUI.java
+++ src/main/java/dap/spotifyAPI/strategy/MainGUI.java
@@ -67,21 +67,19 @@ public class MainGUI {
         String client_id = "75399c2bdb7948b882f6647795204070";
         String client_secret = "484b0ac5151f4288a40c06e8ae7a4dc4";
 
-
         // Pasar el Proxy conectado a la API real
         SpotifyInterface proxy = new Proxy(new Spotify(client_id, client_secret));
         new MainGUI(proxy);
     }
+
+//    public static void main(String[] args) {
+//        String client_id = "75399c2bdb7948b882f6647795204070";
+//        String client_secret = "484b0ac5151f4288a40c06e8ae7a4dc4";
+//
+//        SpotifyInterface spotify = new Spotify(client_id, client_secret);
+//        SpotifyInterface manager = new Proxy(spotify);
+//
+//        String artistId = "grela2235";
+//        System.out.println("Track name: " + manager.getPlaylistsByUser(artistId));
+//    }
 }
-
-
-//public static void main(String[] args) {
-//    String client_id = "75399c2bdb7948b882f6647795204070";
-//    String client_secret = "484b0ac5151f4288a40c06e8ae7a4dc4";
-//
-//    SpotifyInterface spotify = new Spotify(client_id, client_secret);
-//    SpotifyInterface manager = new Proxy(spotify);
-//
-//    String artistId = "grela2235";
-//    System.out.println("Track name: " + manager.getPlaylistsByUser(artistId));
-//}
