cde2c115069098927282445ff8d171b53a76186e
Ramiro <alu0101425030@ull.edu.es>
2025-01-01 15:16:44 +0000

    Proxy y atributos de busqueda añadido a la plantilla

--- src/main/java/dap/spotifyAPI/template/SearchTemplate.java
+++ src/main/java/dap/spotifyAPI/template/SearchTemplate.java
@@ -1,16 +1,20 @@
 package dap.spotifyAPI.template;
 
+import dap.spotifyAPI.proxy.Spotify;
+import dap.spotifyAPI.proxy.SpotifyInterface;
 import se.michaelthelin.spotify.model_objects.specification.Track;
 
 import java.util.List;
 
 public abstract class SearchTemplate {
     protected List<Track> _songs;
-    public abstract void apiCall();
+    protected SpotifyInterface _manager;
+    public abstract void apiCall (String name, String searchField);
     public abstract boolean hasSong();
     public abstract void fetchSong();
-    public final void Search() {
-        apiCall();
+    public final void Search(SpotifyInterface manager, String name, String searchField) {
+        _manager = manager;
+        apiCall(name, searchField);
         if (hasSong()) {
             fetchSong();
         }
