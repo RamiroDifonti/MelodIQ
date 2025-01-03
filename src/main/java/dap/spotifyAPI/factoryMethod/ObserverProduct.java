6a79b8f055413736bf75803a281ac46f76034af6
Ramiro <alu0101425030@ull.edu.es>
2025-01-03 19:13:33 +0000

    arreglando productos del abstract method

--- src/main/java/dap/spotifyAPI/factoryMethod/ObserverProduct.java
+++ src/main/java/dap/spotifyAPI/factoryMethod/ObserverProduct.java
@@ -1,8 +1,13 @@
 package dap.spotifyAPI.factoryMethod;
 
+import dap.spotifyAPI.proxy.SpotifyInterface;
+
+import javax.swing.*;
+
 public class ObserverProduct implements PatternProduct {
     @Override
-    public void display() {
+    public JPanel display(SpotifyInterface manager) {
         System.out.println("Observer Pattern");
+        return new JPanel();
     }
 }
--- src/main/java/dap/spotifyAPI/factoryMethod/PatternProduct.java
+++ src/main/java/dap/spotifyAPI/factoryMethod/PatternProduct.java
@@ -1,5 +1,9 @@
 package dap.spotifyAPI.factoryMethod;
 
+import dap.spotifyAPI.proxy.SpotifyInterface;
+
+import javax.swing.*;
+
 public interface PatternProduct {
-    public void display();
+    public JPanel display(SpotifyInterface manager);
 }
