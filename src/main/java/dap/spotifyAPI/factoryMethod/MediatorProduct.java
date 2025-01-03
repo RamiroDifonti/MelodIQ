49f0929ca22b27b139fbfa28c361e809f8022114
Ramiro <alu0101425030@ull.edu.es>
2025-01-03 19:13:33 +0000

    arreglando productos del abstract method

--- src/main/java/dap/spotifyAPI/factoryMethod/MediatorProduct.java
+++ src/main/java/dap/spotifyAPI/factoryMethod/MediatorProduct.java
@@ -1,8 +1,13 @@
 package dap.spotifyAPI.factoryMethod;
 
+import dap.spotifyAPI.proxy.SpotifyInterface;
+
+import javax.swing.*;
+
 public class MediatorProduct implements PatternProduct {
     @Override
-    public void display() {
+    public JPanel display(SpotifyInterface manager) {
         System.out.println("Mediator Pattern");
+        return new JPanel();
     }
 }
--- src/main/java/dap/spotifyAPI/factoryMethod/StrategyProduct.java
+++ src/main/java/dap/spotifyAPI/factoryMethod/StrategyProduct.java
@@ -1,8 +1,13 @@
 package dap.spotifyAPI.factoryMethod;
 
+import dap.spotifyAPI.proxy.SpotifyInterface;
+
+import javax.swing.*;
+
 public class StrategyProduct implements PatternProduct {
     @Override
-    public void display() {
+    public JPanel display(SpotifyInterface manager) {
         System.out.println("Strategy Pattern");
+        return new JPanel();
     }
 }
